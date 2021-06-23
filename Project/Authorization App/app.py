7fec39cd57ea4b325a31532478f770931dedcfb1
shubham birmi <39634733+code93@users.noreply.github.com>
2021-06-23 13:53:16 +0530

    App for Authorization
     authenticating users login and logging them out, while displaying a message if they are logged in and a person can not go to any other page by modifying the url if I specify @login_required and also the password is being stored as hash in the db.

---
+++ Project/Authorization App/app.py
@@ -0,0 +1,64 @@
+from flask import Flask,render_template,request,redirect
+from flask_login import login_required, current_user, login_user, logout_user
+from models import UserModel,db,login
+ 
+app = Flask(__name__)
+app.secret_key = 'xyz'
+ 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+ 
+ 
+db.init_app(app)
+login.init_app(app)
+login.login_view = 'login'
+ 
+@app.before_first_request
+def create_all():
+    db.create_all()
+     
+@app.route('/blogs')
+@login_required
+def blog():
+    return render_template('blog.html')
+ 
+ 
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if current_user.is_authenticated:
+        return redirect('/blogs')
+     
+    if request.method == 'POST':
+        email = request.form['email']
+        user = UserModel.query.filter_by(email = email).first()
+        if user is not None and user.check_password(request.form['password']):
+            login_user(user)
+            return redirect('/blogs')
+     
+    return render_template('login.html')
+ 
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if current_user.is_authenticated:
+        return redirect('/blogs')
+     
+    if request.method == 'POST':
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+ 
+        if UserModel.query.filter_by(email=email).first():
+            return ('Email already Present')
+             
+        user = UserModel(email=email, username=username)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/login')
+    return render_template('register.html')
+ 
+ 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect('/blogs')
--- Project/Website/app.py
+++ Project/Website/app.py
@@ -1,13 +1,69 @@
+import os
+from re import UNICODE
 import sqlite3
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, url_for, flash, Response
+from flask_login import LoginManager,UserMixin, login_required, login_user, logout_user, current_user
+from forms import LoginForm
+
+
 
 
 app = Flask(__name__)
+app.debug = True
+
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 conn=sqlite3.connect("froshims.db",check_same_thread=False)
 cur= conn.cursor()
-cur.execute('''CREATE TABLE IF NOT EXISTS registrants(email TEXT, password TEXT)''')
+cur.execute('''CREATE TABLE IF NOT EXISTS login(user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+email text not null,
+password text not null);''')
 conn.commit()
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+class User(UserMixin):
+    def __init__(self, id, email, password):
+         self.id = UNICODE(id)
+         self.email = email
+         self.password = password
+         self.authenticated = False
+    def is_active(self):
+         return self.is_active()
+    def is_anonymous(self):
+         return False
+    def is_authenticated(self):
+         return self.authenticated
+    def is_active(self):
+         return True
+    def get_id(self):
+         return self.id
+
+
+@login_manager.user_loader
+def load_user(user_id):
+   conn = sqlite3.connect('froshims.db')
+   curs = conn.cursor()
+   curs.execute("SELECT * from login where id = (?)",[user_id])
+   lu = curs.fetchone()
+   if lu is None:
+      return None
+   else:
+      return User(int(lu[0]), lu[1], lu[2])
+
+
+
+@app.route("/hello")
+def hello():
+    if current_user.is_authenticated:
+        return 'Hello %s!' % current_user.name
+    else:
+        return 'You are not logged in!'
+
+
 
 
 
@@ -22,9 +78,30 @@ def Homepage():
 
 
 
-@app.route("/Login")
+@app.route("/Login", methods=["get","post"])
 def Login():
-    return render_template("Login.html")
+    if current_user.is_authenticated:
+        return redirect(url_for('profile'))
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        conn = sqlite3.connect('froshims.db')
+    
+        curs = conn.cursor()
+        curs.execute("SELECT * FROM login where email = (?)",    [form.email.data])
+        user = list(curs.fetchone())
+        Us = load_user(user[0])
+        if form.email.data == Us.email and form.password.data == Us.password:
+            login_user(Us, remember=form.remember.data)
+            Umail = list({form.email.data})[0].split('@')[0]
+            flash('Logged in successfully '+Umail)
+            redirect(url_for('profile'))
+        else:
+            flash('Login Unsuccessfull.')
+    return render_template('Login.html',title='Login', form=form)
+
+       
 
 @app.route("/Signup")
 def Signup():
@@ -36,7 +113,7 @@ def Contact():
     return render_template("Contact.html")
 
 
-@app.route("/register", methods=["POST"])
+@app.route("/register", methods=["GET","POST"])
 def register():
 
     email = request.form.get("email")
@@ -49,12 +126,16 @@ def register():
 
     df = (email, password)
     
-    cur.execute('''INSERT INTO registrants(email,password) VALUES(?,?)''', df)
+    cur.execute('''INSERT INTO login(email,password) VALUES(?,?)''', df)
 
     return redirect("/registrants")
 
 
 @app.route("/registrants")
 def registrants():
-    registrants = cur.execute('''SELECT * FROM registrants''')
+    registrants = cur.execute('''SELECT * FROM login''')
     return render_template("success.html", message="you are Logged IN!")
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0',port=8080,threaded=True)
